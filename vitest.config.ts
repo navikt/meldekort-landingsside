d8f132a3c142908e58f0ff8c881fbb332f655580
christinaroise <christina.roise.muntean@nav.no>
2026-04-10 13:34:48 +0200

    fix etter kommentarer

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: ['node_modules/', 'dist/', '**/*.config.{js,ts}', '**/*.d.ts', 'src/scripts/'],
+      exclude: ['node_modules/**', 'dist/**', '**/*.config.{js,ts}', '**/*.d.ts', 'src/scripts/**'],
     },
   },
 });
