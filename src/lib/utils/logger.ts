4a9b0ac88db89c814ecd561ebc2c571a85acef96
christinaroise <christina.roise.muntean@nav.no>
2026-04-16 10:50:58 +0200

    utvid logformat i dev

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -1,6 +1,16 @@
 import winston from 'winston';
 
 export const logger = winston.createLogger({
+  level: 'info', // Log alt fra info og oppover (info, warn, error)
   format: process.env.NODE_ENV === 'development' ? winston.format.simple() : winston.format.json(),
-  transports: new winston.transports.Console(),
+  transports: [
+    new winston.transports.Console({
+      format: process.env.NODE_ENV === 'development'
+        ? winston.format.simple()
+        : winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json()
+          ),
+    }),
+  ],
 });
